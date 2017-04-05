9f6681ee1debfd35aa40de07385faeffd6bdaca9
Ahmet Simsek <iamaroott@gmail.com>
2017-04-05 19:19:45 +0300

    object filter

--- elek.js
+++ elek.js
@@ -1,32 +1,52 @@
 import entries from 'object.entries'
+import equal from 'deep-equal'
 
-export default (list, match, layer = 0) => {
-  
+const elek = (list, match, layer = 0) => {
+
   let out = [],
       stack = [],
       rec = (list) => {
-      
+
         for (let [key, value] of entries(list)) {
-  
-          if (value && !(match.match(new RegExp(key)))) {
-            if (typeof value != "string") {
-              stack.push(layer ? {
+          
+          if (typeof match == "object") {
+            
+            if (typeof value == "object" && !equal(match, value)) {
+              rec(value) 
+            } else {
+              out.push(layer ? {
                 [key]: value
               } : value)
-  
-              rec(value)
             }
+            
           } else {
-            out.push(stack.pop())
+
+            if (value && !(match.match(new RegExp(key)))) {
+              if (typeof value != "string") {
+                stack.push(layer ? {
+                  [key]: value
+                } : value)
+  
+                rec(value)
+              }
+            } else {
+              out.push(stack.pop())
+            }
+
           }
-       
+
         }
-      
+
       }
 
-  
   rec(list)
-
-  return out
+  
+  if (typeof match == "object") {
+    return [...new Set(out.filter(o => typeof o === "object"))]
+  } else {
+    return [...new Set(out)]
+  }
 
 }
+
+export default elek
--- test.js
+++ test.js
@@ -16,21 +16,50 @@ const object = {
         "color": "green"
       }
     }
-  }
+  },
+  "xxx1":{
+    "color": "red"
+  },
+  "xxx2": {
+    "color": "blue"
+  },
+  "xxx3": {
+    "color": "green"
+  },
 }
 
 test('layer 0 test', t => {
- 
+  
   t.deepEqual(["red", "blue", "green"],
-      elek(object, "color").map(_ => _.color)
+      [...new Set(elek(object, "color").map(_ => _.color))]
     )
 
 })
 
 test('layer 1 test', t => {
  
-  t.deepEqual(["test1", "test5", "test9"],
+  t.deepEqual(["test1", "test5", "test9", "xxx1", "xxx2", "xxx3"],
       elek(object, "color", 1)
+        .map(_ => Object.keys(_))
+        .reduce((a,b) => a.concat(b)))
+
+})
+
+test('object filter green', t => {
+    
+  t.deepEqual(["test9", "xxx3"],
+      elek(object, { color: 'green' }, 1)
+        .filter(_ => !_.color)
+        .map(_ => Object.keys(_))
+        .reduce((a,b) => a.concat(b)))
+
+})
+
+test('object filter blue', t => {
+    
+  t.deepEqual(["test5", "xxx2"],
+      elek(object, { color: 'blue' }, 1)
+        .filter(_ => !_.color)
         .map(_ => Object.keys(_))
         .reduce((a,b) => a.concat(b)))
 
